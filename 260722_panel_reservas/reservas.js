e3d28e23a3fdb1d2060ef39bef0d38159ddd2ed0
Maria <crispinazarzosa@gmail.com>
2026-07-22 17:43:08 +0200

    actualización reserva

--- 260722_panel_reservas/reservas.js
+++ 260722_panel_reservas/reservas.js
@@ -6,7 +6,7 @@ const btnReserva = document.getElementById('btnReserva')
 const mensajeReserva = document.getElementById('mensajeReserva')
 
 let reservado = false
-
+let plazasDisponibles = 5
 
 // Listeners
 btnReserva.addEventListener('mouseenter', () => {
@@ -28,12 +28,15 @@ btnReserva.addEventListener('mouseleave', ()=>{
 btnReserva.addEventListener('click', () => {
     reservado = !reservado
     if (reservado) {
+        plazasDisponibles--
         btnReserva.textContent = 'Reservado'
         mensajeReserva.textContent = 'Reserva realizada'
         mensajeReserva.classList.add('reservada')
     } else {
+        plazasDisponibles++
         btnReserva.textContent = 'Disponible'
         mensajeReserva.textContent = 'Reserva cancelada'
         mensajeReserva.classList.remove('reservada')
     }
+    numeroPlazas.textContent = plazasDisponibles
 })
