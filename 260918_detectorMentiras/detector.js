256a685986f04340278235b69c41d6e0e7dc38f8
Maria <crispinazarzosa@gmail.com>
2026-09-18 17:05:56 +0200

    detector de mentiras

---
+++ 260918_detectorMentiras/detector.js
@@ -0,0 +1,51 @@
+const video = document.getElementById('video')
+const btnAnalizar = document.getElementById('btnAnalizar')
+const resultado = document.getElementById('resultado')
+const barra = document.getElementById('barra')
+const progreso = document.getElementById('progreso')
+
+
+btnAnalizar.addEventListener('click', () => {
+    console.log('Botón clicado')
+    resultado.textContent = 'Analizando...'
+    barra.style.display = 'block'
+    progreso.style.width = '0%'
+    let porcentaje = 0
+    const intervalo = setInterval(() => {
+        porcentaje += 2
+        progreso.style.width = `${porcentaje}%`
+        if (porcentaje >= 100) {
+            clearInterval(intervalo)
+        }
+    }, 40)
+
+    setTimeout(() => {
+        // resultado.textContent = 'Análisis terminado'
+        const resultados = [
+            'Dice la verdad, fijo',
+            'Miente más que habla',
+            'Resultado incierto. El sujeto desvió la mirada',
+            '100% verdad',
+            'Pupilas dilatadas, trola total',
+            'Datos alterados, no es humano'
+        ]
+        const elegido = resultados[Math.floor(Math.random() * resultados.length)]
+        barra.style.display = 'none'
+        resultado.textContent = elegido
+    }, 2000)
+})
+
+
+
+resultado.textContent = 'Detector preparado'
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    //el navegador tiene una API que se llama mediaDevices, y tiene un método que es getUserMedia,permite pedir acceso a cámara y/o micro
+    .then(transmision => {
+        console.log('Cámara activa')
+        video.srcObject = transmision
+    })
+    .catch(error => {
+        alert('No se puede acceder a la cámara')
+        console.error(error)
+    })
